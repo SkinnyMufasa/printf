bc304294807d58e24f75306df8cb8d2d9adb6856
Kelly Mahlangu <kellymahlangu01@gmail.com>
2023-08-22 15:01:12 +0200

    functions.c

--- functions.c
+++ functions.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
  * print_number - prints an integer
  * @num: The number to be printed
  *
  * Return: Number of digits printed
  */
-
 void print_number(int num)
 {
 	if (num < 0)
@@ -35,52 +33,44 @@ void digit_count(int n)
 		count++;
 	}
 }
-
 /**
  * check_fmt - checks the format of a character after a '%' in a string
  * @args: the vaa_list
  * @fmt: format character
- *
+ * 
  * Return: count
 */
-int check_fmt(va_list args, char fmt)
+int check_fmt(va_list args, char fmt )
 {
 	int count = 0;
-	char *str;
-
+	
 	switch (fmt)
 	{
 		case 'c':
-			if (sizeof(va_arg(args, char)) == 1)
+			if(sizeof(va_arg(args, char)) == 1)
 			{
-				_putchar(va_arg(args, int));
+				count += _putchar(va_arg(args, int));
 			}
 			break;
-		case 's' :
-			str = va_arg(args, char *);
-			if (str == NULL)
-				str = "(null)";
-			while (*str != '\0')
+		case 's':
+			if(sizeof(va_arg(args, char *)) == 8)
 			{
-				count += print_string(str);
-				str++;
+				_puts(va_arg(args, char *));
 			}
 			break;
 		case '%':
-			count++;
-			_putchar('%');
+			count += _putchar(_putchar(fmt));
 			break;
 		case 'i':
 		case 'd':
-			if (sizeof(va_arg(args, int)) == 4)
+			if(sizeof(va_arg(args, int)) == 4)
 			{
 				print_number(va_arg(args, int));
 			}
 			break;
 		default:
-			_putchar(fmt);
+			count += _putchar(fmt);
 			break;
 	}
 	return (count);
 }
-
