5d319f0e1ba044f22948ab2eeacc8d6f3eb84207
Tholoana <tholoana.manyane@gmail.com>
2023-08-22 03:51:59 -0700

    debugging 2.0

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+
+void print_number(int num);
+void digit_count(int n);
+int _putchar(char c);
+int _printf(const char *format, ...);
+int _puts(char *str);
+void print_unsignedint(unsigned int num);
+int check_fmt(va_list args, char fmt );
+int countstr(const char *str);
+
+#endif
+
